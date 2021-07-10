d94b0216483c7d081ed901916420dddfc49dd823
Miraj Rahman <mirajrahman9@gmail.com>
2021-07-10 21:30:25 +0600

    Null Vs Undifined Clear

---
+++ true-false.js
@@ -0,0 +1,20 @@
+// falsy
+// ""
+// undefined
+// null
+// NaN
+// false
+
+// truthy
+// '0' " "
+// []
+// {}
+
+let name = false;
+if (name) {
+    console.log('Condition is true');
+}
+
+else {
+    console.log('Condidtion is false');
+}
---
+++ undefined-null.js
@@ -0,0 +1,20 @@
+let pakhi;
+console.log(pakhi);
+
+function add(num1, num2) {
+    console.log(num1 + num2);
+    return
+}
+
+function add(num1, num2) {
+    console.log(num1, num2);
+}
+
+const man = { name: 'Dude', phone: 0354 }
+console.log(man.salary);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 5, 9];
+console.log(ages[11]);
